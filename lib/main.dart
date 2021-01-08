1bbe1e3259d38349494386f6dcdf4445a861a2c0
18050623009 <47803981+saktiabadi@users.noreply.github.com>
2021-01-08 14:47:04 +0700

    laundry
    laundry

---
+++ lib/main.dart
@@ -0,0 +1,154 @@
+import 'package:flutter/material.dart';
+import 'package:foodorderapp/Style.dart';
+import 'package:foodorderapp/HotelPage.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily: 'mont'),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            size: 27,
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.search,
+              size: 27,
+              color: Colors.grey,
+            ),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                placesWidget("hotel1", "X Laundry"),
+                SizedBox(
+                  height: 20,
+                ),
+                placesWidget("hotel2", "Y Laundry"),
+                SizedBox(
+                  height: 20,
+                ),
+                placesWidget("hotel3", "Y Laundry"),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Row placesWidget(String img, String name) {
+    return Row(
+      children: [
+        Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+              image:
+                  DecorationImage(image: AssetImage("asset/images/$img.png"))),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "$name",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    size: 20,
+                    color: Colors.orange,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 20,
+                    color: Colors.orange,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 20,
+                    color: Colors.orange,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 20,
+                    color: Colors.orange,
+                  ),
+                  Icon(
+                    Icons.star,
+                    size: 20,
+                    color: Colors.orange,
+                  ),
+                ],
+              ),
+              Text(
+                "Lorem ipsum sits dolar amet is for publishing",
+                style: TextStyle(fontSize: 12),
+              )
+            ],
+          ),
+        ),
+        InkWell(
+          onTap: openHotelPage,
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(40)),
+                color: greenBtn),
+            child: Text(
+              "Pesan",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 12,
+                  fontWeight: FontWeight.w700),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  void openHotelPage() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => HotelPage()));
+  }
+}
